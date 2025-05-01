337dc232818de9f7e73023c5e7e72cbf577cc041
Jose Victor Ferreira <josevictorferreira@outlook.com>
2025-05-01 15:07:31 -0300

    added blink configs

--- config/nvim/lua/plugins/blink.lua
+++ config/nvim/lua/plugins/blink.lua
@@ -1,17 +1,45 @@
 return {
 	"saghen/blink.cmp",
-	dependencies = { "L3MON4D3/LuaSnip" },
+	dependencies = { "L3MON4D3/LuaSnip", version = "v2.*" },
 	version = "1.*",
 	opts = {
-		keymap = { preset = "default" },
+		keymap = {
+			["C-y"] = {},
+			["<C-space>"] = { "show", "show_documentation", "hide_documentation" },
+			["<C-e>"] = { "hide" },
+			["<Tab>"] = { "select_and_accept" },
+			["<Up>"] = { "select_prev", "fallback" },
+			["<Down>"] = { "select_next", "fallback" },
+			["<C-p>"] = { "select_prev", "fallback_to_mappings" },
+			["<C-n>"] = { "select_next", "fallback_to_mappings" },
+
+			["<C-d>"] = { "scroll_documentation_up", "fallback" },
+			["<C-f>"] = { "scroll_documentation_down", "fallback" },
+			["<C-k>"] = { "show_signature", "hide_signature", "fallback" },
+		},
 		appearance = {
 			nerd_font_variant = "mono",
 		},
-		completion = { documentation = { auto_show = false } },
-		sources = {
-			default = { "lsp", "path", "snippets", "buffer" },
+		snippets = { preset = "luasnip" },
+		completion = {
+			documentation = {
+				auto_show = true,
+				border = { "double" },
+			},
+			menu = { border = "single" },
 		},
-		fuzzy = { implementation = "prefer_rust_with_warning" },
+		sources = {
+			default = { "lsp", "snippets", "path", "buffer" },
+		},
+		signature = { enabled = true },
+		fuzzy = {
+			implementation = "prefer_rust_with_warning",
+			sorts = {
+				"exact",
+				"score",
+				"sort_text",
+			},
+		},
 	},
 	opts_extend = { "sources.default" },
 }
