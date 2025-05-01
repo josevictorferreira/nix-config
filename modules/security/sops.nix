9a9c500d96c83814cbe828d3ba424a1ee354f302
Jose Victor Ferreira <josevictorferreira@outlook.com>
2025-05-01 19:06:34 -0300

    updated sops

--- modules/security/sops.nix
+++ modules/security/sops.nix
@@ -1,16 +1,22 @@
-{ config, username, configRoot, isDarwin, ... }:
+{ config, username, configRoot, ... }:
 
 let
-  homeDirPrefix = if isDarwin then "/Users" else "/home";
+  ageKeyFilePath = "${config.users.users.${username}.home}/.config/sops/age/keys.txt";
 in
 {
   sops = {
     defaultSopsFile = "${configRoot}/secrets/secrets.enc.yaml";
-    age.keyFile = "${homeDirPrefix}/${username}/.config/sops/age/keys.txt";
+    age.keyFile = ageKeyFilePath;
   };
 
   sops.secrets."anthropic_api_key" = {
     owner = config.users.users.${username}.name;
     mode = "0400";
+  };
+
+  environment = {
+    variables = {
+      SOPS_AGE_KEY_FILE = ageKeyFilePath;
+    };
   };
 }
