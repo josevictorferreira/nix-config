c99c7e9a681cd78b85224c19d518036c8b12a2db
Jose Victor Ferreira <josevictorferreira@outlook.com>
2025-05-08 16:20:46 -0300

    Loggin lockscreen activations

--- config/hypr/scripts/LockScreen.sh
+++ config/hypr/scripts/LockScreen.sh
@@ -2,5 +2,6 @@
 # /* ---- 💫 https://github.com/JaKooLit 💫 ---- */  ##
 
 # For Hyprlock
+echo "LockScreen.sh triggered at $(date)" >> /tmp/lockscreen.log
 
 pidof hyprlock || hyprlock -c $XDG_CONFIG_HOME/hypr/hyprlock-2k.conf -q 
