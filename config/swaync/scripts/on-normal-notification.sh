892354eccf5193bac29cd9648901872bf7de8fb3
Jose Victor Ferreira <root@josevictor.me>
2025-06-09 14:49:26 -0300

    updated swaync to trigger sound notification, added critical sound

---
+++ config/swaync/scripts/on-normal-notification.sh
@@ -0,0 +1,3 @@
+#!/run/current-system/sw/bin/bash
+
+pw-play ~/.config/swaync/sounds/normal_notification.ogg &>/dev/null &
--- config/swaync/scripts/on-notification.sh
+++
@@ -1,3 +0,0 @@
-#!/run/current-system/sw/bin/bash
-
-pw-play ~/.config/swaync/sounds/new_notification.ogg &>/dev/null &
