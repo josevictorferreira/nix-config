88d0f2b956f876f58f2270a312b5a150298f536d
Jose Victor Ferreira <root@josevictor.me>
2025-08-24 20:14:57 -0300

    updated homelab storage

--- modules/hardware/homelab-storage.nix
+++ modules/hardware/homelab-storage.nix
@@ -1,11 +1,21 @@
 { lib, username, isDarwin, ... }:
 
+let mnt = "/mnt/homelab"; in
 {
   fileSystems = {
-    "/home/${username}/homelab" = lib.mkIf (!isDarwin) {
+    "${mnt}" = lib.mkIf (!isDarwin) {
       device = "10.10.10.150:/homelab";
       fsType = "nfs4";
       options = [ "vers=4.2" "rw" "hard" "noatime" "timeo=600" "actimeo=60" "retrans=2" "x-systemd.automount" ];
     };
   };
+
+  system.activationScripts.homelabLink = ''
+    install -d -o ${username} -g users /home/${username}
+    ln -snf ${mnt} /home/${username}/homelab
+    chown -h ${username}:users /home/${username}/homelab
+  '';
+
+  users.groups.homelab.gid = 2002;
+  users.users.${username}.extraGroups = [ "homelab" ];
 }
