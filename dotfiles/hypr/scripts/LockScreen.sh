42af4493bbcca6f67ee7a2530e65490a88260238
Jose Victor Ferreira <root@josevictor.me>
2025-09-24 14:19:45 -0300

    Add hypridle configuration with custom lock and sleep commands, update hyprlock-2k.conf with current wallpaper and date display, and include lock screen script modifications

--- dotfiles/hypr/scripts/LockScreen.sh
+++ dotfiles/hypr/scripts/LockScreen.sh
@@ -2,4 +2,6 @@
 # /* ---- 💫 https://github.com/JaKooLit 💫 ---- */  ##
 
 # For Hyprlock
-pidof hyprlock || hyprlock -c "$HOME/.config/hypr/hyprlock-2k.conf"
+# pidof hyprlock || hyprlock -c "$HOME/.config/hypr/hyprlock-2k.conf"
+
+loginctl lock-session
