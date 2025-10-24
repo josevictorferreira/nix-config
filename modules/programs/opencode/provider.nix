ee66849ed25c377238709f52aceafd638f6f5519
Jose Victor Ferreira <root@josevictor.me>
2025-10-24 08:32:40 -0300

    feat: Add new AI model providers for opencode
    Several new AI model providers have been added to `opencode` to expand its capabilities and support more advanced models. This includes `glm-4.6:exacto`, `kimi-k2-0905:exacto`, `deepseek/deepseek-v3.1-terminus:exacto`, and `qwen/qwen3-coder:exacto`.
    
    Additionally, autoscrolling has been disabled for `k9s` to prevent unexpected behavior during usage.

--- modules/programs/opencode/provider.nix
+++ modules/programs/opencode/provider.nix
@@ -3,14 +3,20 @@
   config.jvf.programs.opencode.settings.provider = {
     openrouter = {
       models = {
-        "z-ai/glm-4.6" = {
-          name = "GLM 4.6";
+        "z-ai/glm-4.6:exacto" = {
+          name = "GLM 4.6 Exacto";
         };
         "x-ai/grok-4-fast" = {
           name = "Grok 4 Fast";
         };
-        "moonshotai/kimi-k2-0905" = {
-          name = "Kimi K2 Instruct 0905";
+        "moonshotai/kimi-k2-0905:exacto" = {
+          name = "Kimi K2 Instruct 0905 Exacto";
+        };
+        "deepseek/deepseek-v3.1-terminus:exacto" = {
+          name = "DeepSeek v3.1 Exacto";
+        };
+        "qwen/qwen3-coder:exacto" = {
+          name = "Qwen3 Coder Exacto";
         };
         "google/gemini-2.5-pro" = {
           name = "Gemini 2.5 Pro";
