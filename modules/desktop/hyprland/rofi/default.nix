bdd815657dd41cc8115b281198292bb2dc64066f
Jose Victor Ferreira <root@josevictor.me>
2025-11-08 20:30:38 -0300

    Updated rofi configuration files

--- modules/desktop/hyprland/rofi/default.nix
+++ modules/desktop/hyprland/rofi/default.nix
@@ -1,7 +1,8 @@
-{ lib
-, config
-, pkgs
-, ...
+{
+  lib,
+  config,
+  pkgs,
+  ...
 }:
 with lib;
 let
@@ -13,34 +14,13 @@ in
   };
 
   config = mkIf cfg.enable {
-    environment.systemPackages = with pkgs; [
-      rofi
-    ];
-
-    environment.etc = {
-      "xdg/rofi.rasi".text = import ./config/main.nix { inherit config pkgs; };
-      "xdg/rofi/master-config.rasi".text = import ./config/master.nix { inherit config pkgs; };
-      "xdg/rofi/config-calc.rasi".text = import ./config/calc.nix { inherit config pkgs; };
-      "xdg/rofi/config-clipboard.rasi".text = import ./config/clipboard.nix { inherit config pkgs; };
-      "xdg/rofi/config-compact.rasi".text = import ./config/compact.nix { inherit config pkgs; };
-      "xdg/rofi/config-emoji.rasi".text = import ./config/emoji.nix { inherit config pkgs; };
-      "xdg/rofi/config-keybinds.rasi".text = import ./config/keybinds.nix { inherit config pkgs; };
-      "xdg/rofi/config-rofi-Beats-menu.rasi".text = import ./config/rofi-beats-menu.nix {
-        inherit config pkgs;
+    jvf.wrappers.users.${cfg.username}.programs.rofi = {
+      packages = [
+        pkgs.rofi
+      ];
+      configs = {
+        "rofi" = ./.;
       };
-      "xdg/rofi/config-rofi-Beats.rasi".text = import ./config/rofi-beats.nix { inherit config pkgs; };
-      "xdg/rofi/config-search.rasi".text = import ./config/search.nix { inherit config pkgs; };
-      "xdg/rofi/config-wallpaper-effect.rasi".text = import ./config/wallpaper-effect.nix {
-        inherit config pkgs;
-      };
-      "xdg/rofi/config-wallpaper.rasi".text = import ./config/wallpaper.nix { inherit config pkgs; };
-      "xdg/rofi/config-waybar-layout.rasi".text = import ./config/waybar-layout.nix {
-        inherit config pkgs;
-      };
-      "xdg/rofi/config-waybar-style.rasi".text = import ./config/waybar-style.nix {
-        inherit config pkgs;
-      };
-      "xdg/rofi/config-zsh-theme.rasi".text = import ./config/zsh-theme.nix { inherit config pkgs; };
     };
   };
 }
