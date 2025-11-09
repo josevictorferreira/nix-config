c3c9a881fd68c680f280e663477c5991eae19352
Jose Victor Ferreira <root@josevictor.me>
2025-11-09 10:56:27 -0300

    feat: Configure AGS for user
    This commit refactors the AGS configuration to be user-specific.
    
    Previously, AGS was configured globally. By making it user-specific, we can now manage AGS configurations per user, which is a more flexible and maintainable approach. This change aligns with managing user-specific desktop environments and their associated applications.

--- modules/desktop/hyprland/ags/default.nix
+++ modules/desktop/hyprland/ags/default.nix
@@ -1,49 +1,33 @@
-{ lib
-, pkgs
-, config
-, ...
+{
+  lib,
+  pkgs,
+  config,
+  username,
+  ...
 }:
 
 let
   cfg = config.jvf.desktop.hyprland.ags;
-
-  agsConfig = pkgs.stdenv.mkDerivation {
-    pname = "ags-config";
-    version = "1.0.0";
-
-    src = lib.cleanSourceWith {
-      src = ./.;
-      filter = path: type: (baseNameOf path) != "default.nix";
-    };
-
-    installPhase = ''
-      mkdir -p $out
-      cp -r ./* $out/
-    '';
-  };
-
-  agsWrapper = pkgs.writeShellApplication {
-    name = "ags";
-    runtimeInputs = [ cfg.package ];
-
-    text = ''
-      exec ${cfg.package}/bin/ags --config ${agsConfig}/config.js "$@"
-    '';
-  };
-
 in
 {
   options.jvf.desktop.hyprland.ags = {
     enable = lib.mkEnableOption "AGS - Awesome Hyprland Widgets";
 
-    package = lib.mkOption {
-      type = lib.types.package;
-      default = pkgs.ags;
-      description = "The AGS package to use.";
+    username = lib.mkOption {
+      type = lib.types.str;
+      default = username;
+      description = "Username to configure AGS for.";
     };
   };
 
   config = lib.mkIf cfg.enable {
-    environment.systemPackages = [ agsWrapper ];
+    jvf.wrappers.users.${cfg.username}.programs.ags = {
+      packages = [
+        pkgs.ags
+      ];
+      configs = {
+        "ags" = ./.;
+      };
+    };
   };
 }
