17d1b0e736c79ca8ea7b156bf38ee0418f6d2e81
Jose Victor Ferreira <root@josevictor.me>
2025-11-21 19:54:46 -0300

    Refactor: Conditionally clone repos and change launchd config
    This commit refactors the repository cloning logic to specifically handle macOS by creating the directory and then cloning if it doesn't exist, using `GIT_SSH_COMMAND` for better SSH integration. It also makes a minor correction to `launchd.daemons` to `launchd.agents`, which is the correct configuration for macOS user agents.

--- modules/users/repositories.nix
+++ modules/users/repositories.nix
@@ -21,11 +21,20 @@ let
     lib.concatStringsSep "\n" (
       lib.mapAttrsToList (
         rel: repo:
-        inputs.lib.git.cloneRepoText {
-          username = userName;
-          inherit group repo;
-          targetDir = "${home}/${rel}";
-        }
+        if pkgs.stdenv.isDarwin then
+          ''
+            if [ ! -d "${home}/${rel}" ]; then
+              ${pkgs.coreutils}/bin/mkdir -p "${home}/${rel}"
+              GIT_SSH_COMMAND="${pkgs.openssh}/bin/ssh -o StrictHostKeyChecking=accept-new" \
+                ${pkgs.git}/bin/git clone --depth=1 ${repo} "${home}/${rel}"
+            fi
+          ''
+        else
+          inputs.lib.git.cloneRepoText {
+            username = userName;
+            inherit group repo;
+            targetDir = "${home}/${rel}";
+          }
       ) uCfg.clonedDirs
     );
 in
@@ -54,7 +63,7 @@ in
   config = lib.mkMerge (
     [ ]
     ++ lib.optional isDarwin {
-      launchd.daemons = lib.mkMerge (
+      launchd.agents = lib.mkMerge (
         lib.mapAttrsToList (
           userName: uCfg:
           if uCfg.clonedDirs == { } then
@@ -74,6 +83,8 @@ in
                   RunAtLoad = true;
                   StandardOutPath = "/tmp/jvf-clone-repos-${userName}.log";
                   StandardErrorPath = "/tmp/jvf-clone-repos-${userName}.err";
+                  UserName = userName;
+                  GroupName = if pkgs.stdenv.isDarwin then "staff" else "users";
                 };
               };
             }
