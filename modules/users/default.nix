22d28882461435dafe68e54694e5be2b522455f6
Jose Victor Ferreira <root@josevictor.me>
2025-11-21 15:41:52 -0300

    Refactor user and group configuration
    This change refactors the user and group configuration to use `lib.mkMerge` and `lib.optionalAttrs`, making the code more declarative and cleaner. It also explicitly defines groups for mutable users on non-Darwin systems, improving clarity and maintainability.

--- modules/users/default.nix
+++ modules/users/default.nix
@@ -70,27 +70,28 @@ in
     };
   };
 
-  config = {
-    users.users = lib.mapAttrs (
-      name: userCfg:
-      lib.mkIf userCfg.enable {
-        description = userCfg.description;
-        openssh.authorizedKeys.keys = userCfg.authorizedKeys;
-        packages = userCfg.packages;
-      }
-      // (
-        if (!isDarwin) then
+  config = lib.mkMerge [
+    {
+      users.users = lib.mapAttrs (
+        name: userCfg:
+        lib.mkIf userCfg.enable (
           {
+            description = userCfg.description;
+            openssh.authorizedKeys.keys = userCfg.authorizedKeys;
+            packages = userCfg.packages;
+          }
+          // lib.optionalAttrs (!isDarwin) {
             isNormalUser = true;
+            group = name;
             homeMode = userCfg.homeMode;
             extraGroups = userCfg.extraGroups;
           }
-        else
-          { }
-      )
-    ) cfg.users;
-  }
-  // lib.optionalAttrs (!isDarwin) {
-    users.mutableUsers = true;
-  };
+        )
+      ) cfg.users;
+    }
+    (lib.mkIf (!isDarwin) {
+      users.mutableUsers = true;
+      users.groups = lib.mapAttrs (name: userCfg: lib.mkIf userCfg.enable { }) cfg.users;
+    })
+  ];
 }
