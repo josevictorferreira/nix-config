9431d32ae740538f1e6466106689da287eb0a85d
Jose Victor Ferreira <root@josevictor.me>
2025-12-11 16:53:44 -0300

    ✅ Refactor opencode plugin to use pkgs.fetchFromGitHub
    Refactor opencode plugin to use pkgs.fetchFromGitHub to ensure reproducible builds and better dependency management.

--- modules/programs/opencode/plugins.nix
+++ modules/programs/opencode/plugins.nix
@@ -31,9 +31,40 @@ let
       mainProgram = "opencode-skills";
     };
   };
+  openCodeDynamicContextPkg = pkgs.buildNpmPackage rec {
+    pname = "opencode-dynamic-context-pruning";
+    version = "v0.4.17";
+
+    src = pkgs.fetchFromGitHub {
+      owner = "Tarquinen";
+      repo = "opencode-dynamic-context-pruning";
+      rev = "${version}";
+      hash = "sha256-nEbt+/70Fhm7zEBq7j7Ay4jwSZNjdfjMNcEX4xfZCug=";
+    };
+
+    npmDepsHash = "sha256-Ma2/bgPS1dYz6WXL1FVHKZW1vVbxfVxCGSJNWg9vX9o=";
+
+    postPatch = ''
+      ${lib.getExe pkgs.jq} '
+        .dependencies += (.peerDependencies // {}) |
+        . as $all | .devDependencies |= with_entries(
+          select(.key as $k | ($all.dependencies | has($k) | not))
+        ) |
+        del(.peerDependencies)
+      ' package.json > package-tmp.json && mv package-tmp.json package.json
+    '';
+
+    meta = with lib; {
+      description = "Opencode Dynamic Context Pruning";
+      homepage = "https://github.com/Tarquinen/opencode-dynamic-context-pruning";
+      license = licenses.mit;
+      mainProgram = "opencode-dynamic-context-pruning";
+    };
+  };
 in
 {
   config.jvf.programs.opencode.settings.plugin = [
     "${openCodeSkillsPkg}/lib/node_modules/opencode-skills/dist/index.js"
+    "${openCodeDynamicContextPkg}/lib/node_modules/@tarquinen/opencode-dcp/dist/index.js"
   ];
 }
