8ea7ec25d84449375ccdd6cf831c0ec4f32a34a8
Jose Victor Ferreira <root@josevictor.me>
2026-01-23 17:38:03 -0300

    💄 Add browser-debug-tools AI skill This commit adds a new AI tool skill for browser automation and debugging.

--- modules/common/ai-tools/skills/default.nix
+++ modules/common/ai-tools/skills/default.nix
@@ -5,6 +5,7 @@
     ./general/auditing-security.nix
     ./general/creating-skills.nix
     ./general/research-tools.nix
+    ./general/browser-debug-tools.nix
     ./container/developing-containers.nix
     ./nix/creating-nix-modules.nix
     ./nix/managing-flakes.nix
