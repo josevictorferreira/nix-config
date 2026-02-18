d7b2f8d9ba05b416b45bfa48ba311a4fbae68122
Jose Victor Ferreira <root@josevictor.me>
2026-02-18 11:56:26 -0300

    ⬆️ Update opencode provider and plugins Update opencode provider and plugins to the latest versions.

--- modules/programs/opencode/plugins.nix
+++ modules/programs/opencode/plugins.nix
@@ -6,9 +6,9 @@
 {
   config.jvf.programs.opencode.settings.plugin = [
     "opencode-antigravity-auth@1.5.1"
-    "oh-my-opencode@3.7.2"
+    "oh-my-opencode@3.7.3"
     "openslimedit@latest"
-    "@tarquinen/opencode-dcp@2.1.3"
+    "@tarquinen/opencode-dcp@2.1.5"
     "@howaboua/opencode-usage-plugin"
   ];
 
