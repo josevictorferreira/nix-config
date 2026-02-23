b29b1e9752ab4b61c10da01c33eecb0d59b837e4
Jose Victor Ferreira <root@josevictor.me>
2026-02-22 22:44:06 -0300

    feat: add hardware-boot and hardware-btrfs dendritic aspects
    Extract reusable boot config (grub, plymouth, binfmt, kernel) and
    btrfs autoScrub into dendritic aspects under modules/hardware/.

---
+++ modules/hardware/boot.nix
@@ -0,0 +1,184 @@
+# Aspect: hardware-boot
+# Boot configuration: kernel, grub, plymouth, binfmt, sysctl, firmware.
+# NixOS-only (no Darwin equivalent).
+{ ... }:
+let
+  mkBootOptions =
+    { config, lib, ... }:
+    {
+      options.jvf.hardware.boot = {
+        enable = lib.mkEnableOption "Boot configuration" // {
+          description = ''
+            Whether to enable boot configuration with zen kernel,
+            grub, plymouth, binfmt (appimage), and system tweaks.
+          '';
+        };
+
+        kernel = lib.mkOption {
+          type = lib.types.enum [
+            "zen"
+            "latest"
+            "lts"
+          ];
+          default = "zen";
+          description = "Kernel variant to use.";
+        };
+
+        kernelModules = lib.mkOption {
+          type = lib.types.listOf lib.types.str;
+          default = [ "kvm-amd" ];
+          description = "Additional kernel modules to load.";
+        };
+
+        grub = {
+          enable = lib.mkOption {
+            type = lib.types.bool;
+            default = true;
+            description = "Enable GRUB bootloader.";
+          };
+          configurationLimit = lib.mkOption {
+            type = lib.types.int;
+            default = 10;
+            description = "Number of boot entries to keep.";
+          };
+          useOSProber = lib.mkOption {
+            type = lib.types.bool;
+            default = true;
+            description = "Enable os-prober for detecting other OSes.";
+          };
+        };
+
+        plymouth = {
+          enable = lib.mkOption {
+            type = lib.types.bool;
+            default = true;
+            description = "Enable Plymouth boot splash.";
+          };
+          theme = lib.mkOption {
+            type = lib.types.str;
+            default = "bgrt";
+            description = "Plymouth theme to use.";
+          };
+        };
+
+        binfmt = {
+          enable = lib.mkOption {
+            type = lib.types.bool;
+            default = true;
+            description = "Enable binfmt for appimage support.";
+          };
+        };
+
+        firmware = {
+          enable = lib.mkOption {
+            type = lib.types.bool;
+            default = true;
+            description = "Enable redistributable firmware and linux-firmware package.";
+          };
+        };
+
+        i2c = {
+          enable = lib.mkOption {
+            type = lib.types.bool;
+            default = true;
+            description = "Enable I2C hardware support.";
+          };
+        };
+      };
+    };
+
+  mkConfig =
+    { config
+    , lib
+    , pkgs
+    , ...
+    }:
+    let
+      cfg = config.jvf.hardware.boot;
+      kernelPackages =
+        if cfg.kernel == "zen" then
+          pkgs.linuxPackages_zen
+        else if cfg.kernel == "latest" then
+          pkgs.linuxPackages_latest
+        else
+          pkgs.linuxPackages;
+    in
+    {
+      imports = [ mkBootOptions ];
+
+      config = lib.mkIf cfg.enable {
+        boot = {
+          kernelModules = cfg.kernelModules;
+          extraModulePackages = [ ];
+          kernelPackages = kernelPackages;
+
+          kernelParams = [
+            "quiet"
+            "loglevel=3"
+            "systemd.show_status=auto"
+            "udev.log_level=3"
+            "rd.udev.log_level=3"
+            "vt.global_cursor_default=0"
+            "boot.shell_on_fail"
+            "plymouth.use-simpledrm"
+          ];
+
+          initrd.verbose = false;
+
+          kernel.sysctl = {
+            "vm.max_map_count" = 2147483642;
+          };
+
+          loader = {
+            systemd-boot.enable = false;
+            efi.canTouchEfiVariables = true;
+            timeout = 1;
+
+            grub = lib.mkIf cfg.grub.enable {
+              enable = true;
+              device = "nodev";
+              efiSupport = true;
+              gfxmodeBios = "auto";
+              memtest86.enable = true;
+              configurationLimit = cfg.grub.configurationLimit;
+              useOSProber = cfg.grub.useOSProber;
+            };
+          };
+
+          tmp = {
+            useTmpfs = false;
+            tmpfsSize = "30%";
+          };
+
+          binfmt.registrations = lib.mkIf cfg.binfmt.enable {
+            appimage = {
+              wrapInterpreterInShell = false;
+              interpreter = "${pkgs.appimage-run}/bin/appimage-run";
+              recognitionType = "magic";
+              offset = 0;
+              mask = ''\xff\xff\xff\xff\x00\x00\x00\x00\xff\xff\xff'';
+              magicOrExtension = ''\x7fELF....AI\x02'';
+            };
+          };
+
+          consoleLogLevel = 0;
+
+          plymouth = lib.mkIf cfg.plymouth.enable {
+            enable = true;
+            theme = cfg.plymouth.theme;
+          };
+        };
+
+        console.earlySetup = false;
+
+        hardware = {
+          enableRedistributableFirmware = cfg.firmware.enable;
+          firmware = lib.mkIf cfg.firmware.enable [ pkgs.linux-firmware ];
+          i2c.enable = cfg.i2c.enable;
+        };
+      };
+    };
+in
+{
+  flake.modules.nixos.hardware-boot = mkConfig;
+}
---
+++ modules/hardware/btrfs.nix
@@ -0,0 +1,65 @@
+# Aspect: hardware-btrfs
+# Btrfs filesystem support: autoScrub, btrfs-progs.
+# NixOS-only (no Darwin equivalent).
+{ ... }:
+let
+  mkBtrfsOptions =
+    { lib, ... }:
+    {
+      options.jvf.hardware.btrfs = {
+        enable = lib.mkEnableOption "Btrfs filesystem support" // {
+          description = ''
+            Whether to enable Btrfs support with auto-scrub and tools.
+          '';
+        };
+
+        autoScrub = {
+          enable = lib.mkOption {
+            type = lib.types.bool;
+            default = true;
+            description = "Enable automatic btrfs scrub.";
+          };
+          interval = lib.mkOption {
+            type = lib.types.str;
+            default = "monthly";
+            description = "How often to run btrfs scrub.";
+          };
+          fileSystems = lib.mkOption {
+            type = lib.types.listOf lib.types.str;
+            default = [ "/" ];
+            description = "Which filesystems to scrub.";
+          };
+        };
+      };
+    };
+
+  mkConfig =
+    { config
+    , lib
+    , pkgs
+    , ...
+    }:
+    let
+      cfg = config.jvf.hardware.btrfs;
+    in
+    {
+      imports = [ mkBtrfsOptions ];
+
+      config = lib.mkIf cfg.enable {
+        boot.supportedFilesystems = [ "btrfs" ];
+
+        environment.systemPackages = [
+          pkgs.btrfs-progs
+        ];
+
+        services.btrfs.autoScrub = lib.mkIf cfg.autoScrub.enable {
+          enable = true;
+          interval = cfg.autoScrub.interval;
+          fileSystems = cfg.autoScrub.fileSystems;
+        };
+      };
+    };
+in
+{
+  flake.modules.nixos.hardware-btrfs = mkConfig;
+}
