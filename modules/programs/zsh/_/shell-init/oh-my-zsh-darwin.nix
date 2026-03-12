43266c65c1698b48564584dbd4f3c33d99c02b50
Jose Victor Ferreira <root@josevictor.me>
2026-03-11 22:32:15 -0300

    feat(zsh): Disable oh-my-zsh ls colors in macOS
    Prevents oh-my-zsh from overriding `eza` aliases by disabling its colorization for `ls` commands. This ensures consistent aliasing for directory listings on macOS.

--- modules/programs/zsh/_/shell-init/oh-my-zsh-darwin.nix
+++ modules/programs/zsh/_/shell-init/oh-my-zsh-darwin.nix
@@ -18,6 +18,8 @@ in
 
       # Disable oh-my-zsh auto-update (managed by Nix)
       DISABLE_AUTO_UPDATE="true"
+      # Disable oh-my-zsh ls colors to prevent it from overriding our eza aliases
+      DISABLE_LS_COLORS="true"
 
       if [ -f $ZSH/oh-my-zsh.sh ]; then
         source $ZSH/oh-my-zsh.sh
