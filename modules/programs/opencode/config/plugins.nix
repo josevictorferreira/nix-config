328f2203c01603e48b5655d3483600aa1d78a2a4
Jose Victor Ferreira <root@josevictor.me>
2026-07-26 19:47:11 -0300

    Refactor: Update opencode oh-my-openagent plugin to v4.19.2
    Updates the installed version of the `oh-my-openagent` plugin for opencode to leverage the latest bug fixes and improvements in v4.19.2.

--- modules/programs/opencode/config/plugins.nix
+++ modules/programs/opencode/config/plugins.nix
@@ -4,7 +4,7 @@ _: {
     jvf.programs.opencode.settings = {
       plugin = [
         "@tarquinen/opencode-dcp@3.1.14"
-        "oh-my-openagent@4.19.1"
+        "oh-my-openagent@4.19.2"
         "@vectorize-io/opencode-hindsight"
       ];
     };
